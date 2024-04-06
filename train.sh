04fb319b803dbc3896c046f631fec0e7fc329305
Tao Tang <84534481+TangTao-PKU@users.noreply.github.com>
2024-04-06 15:57:13 +0800

    Update

--- train.sh
+++ train.sh
@@ -3,8 +3,8 @@
 
 # Table1 3dpw
 python main.py --cfg ./configs/3dpw_table2.yaml \
---gpu 2 --model trans_t
+--gpu 2 --model DGTR
 
 # python main.py --cfg ./configs/3dpw_table2.yaml \
-# --gpu 1 --model trans_t
+# --gpu 1 --model DGTR
 
